93d10359c2052e8db8b0422aa3ce05f1950c72a8
Sergei <stagephoning@yandex.ru>
2021-09-03 10:40:54 +0300

    swap to postgres

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodgram',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',#5432
     }
 }
 
