4e76330d78b5fc488c6883fef03870a85b85c4b9
Sergei <stagephoning@yandex.ru>
2021-09-05 22:14:42 +0300

    add endpoint

--- backend/apps/recipes/urls.py
+++ backend/apps/recipes/urls.py
@@ -11,8 +11,16 @@ router_v1.register(r'recipes', views.RecipeViewSet, basename='recipes')
 router_v1.register(
     r'ingredients', views.IngredientViewSet, basename='ingredients'
 )
-router_v1.register(r'recipes', views.RecipeViewSet, basename='recipes')
-
+router_v1.register(
+    r'recipes/(?P<recipes_id>\d+)/favorite/',
+    views.FavoriteViewSet,
+    basename='favorite',
+)
+router_v1.register(
+    r'recipes/download_shopping_cart/',
+    views.PurchaseViewSet,
+    basename='purchase',
+)
 urlpatterns = [
     path('', include(router_v1.urls)),
 ]
