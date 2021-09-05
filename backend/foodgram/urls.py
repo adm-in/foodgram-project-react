40d78ef858b185297068a643f950b6586e29819b
Sergei <stagephoning@yandex.ru>
2021-09-05 23:05:19 +0300

    add users urls

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(
-        'api/',
-        include('recipes.urls', namespace='recipes')),
+    path('api/', include('recipes.urls', namespace='recipes')),
+    path('api/', include('users.urls', namespace='users')),
     path('auth/', include('djoser.urls')),
     # JWT-эндпоинты, для управления JWT-токенами:
     path('auth/', include('djoser.urls.jwt')),
-
 ]
