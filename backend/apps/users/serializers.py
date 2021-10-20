9a6ed55e316724a1bc1d669bbe36e0157738e24c
Sergei <stagephoning@yandex.ru>
2021-10-20 13:43:56 +0300

    pep8 update

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,9 +1,8 @@
-from djoser.serializers import \
-    UserCreateSerializer as DjoserUserCreateSerializer
+from djoser.serializers import UserCreateSerializer \
+    as DjoserUserCreateSerializer
 from drf_extra_fields.fields import Base64ImageField
 from recipes.models import Recipe
 from rest_framework import serializers
-
 from users.models import CustomUser, Subscribe
 
 
