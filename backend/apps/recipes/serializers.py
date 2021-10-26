d47aa44a476e132bebb95e998b55dd99a41e7b8d
Sergei <stagephoning@yandex.ru>
2021-10-26 13:08:21 +0300

    flake8

--- backend/apps/recipes/serializers.py
+++ backend/apps/recipes/serializers.py
@@ -1,6 +1,5 @@
 from drf_extra_fields.fields import Base64ImageField
-from recipes.models import (Favorite, Ingredient, IngredientRecipe, Purchase,
-                            Recipe, Tag)
+from recipes.models import Ingredient, IngredientRecipe, Recipe, Tag
 from rest_framework import serializers
 from users.serializers import CustomUserSerializer
 
