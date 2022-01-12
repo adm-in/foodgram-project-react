0b3641de7cd94d415a13689e79be198b1df8b5e3
Sergei <stagephoning@yandex.ru>
2022-01-12 14:35:00 +0300

    pep8 tests update

--- backend/tests/test_homepage.py
+++ backend/tests/test_homepage.py
@@ -1,17 +1,18 @@
 import pytest
 
-
 # При GET запросе на адрес /api/recipes/ должен выводится список первых 6
 # рецептов отсортированых по дате побликации. Главная страница доступна всем,
 # в ответ должен приходить 200.1
 
-@pytest.fixture
+
+@pytest.fixture()
 def api_client():
     from rest_framework.test import APIClient
+
     return APIClient()
 
 
-@pytest.mark.django_db
+@pytest.mark.django_db()
 def test_view_unauthorized(api_client):
     url = '/api/recipes/'
     response = api_client.get(url)
--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -4,7 +4,7 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 
-@pytest.mark.django_db
+@pytest.mark.django_db()
 def test_user_create():
     User.objects.create_user(
         username='test_user',
