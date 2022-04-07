74bc5bf76f589412284a7e229c3a2d89099aa1d9
Sergei <stagephoning@yandex.ru>
2022-04-07 16:17:48 +0300

    add fixtures from pytest

--- backend/apps/recipes/permissions.py
+++ backend/apps/recipes/permissions.py
@@ -8,10 +8,9 @@ class AdminOrAuthorOrReadOnly(permissions.BasePermission):
         return True
 
     def has_object_permission(self, request, view, obj):
-        if (request.method in ['PUT', 'PATCH', 'DELETE']
-                and not request.user.is_anonymous):
-            return (
-                request.user == obj.author
-                or request.user.is_superuser
-            )
+        if (
+            request.method in ['PUT', 'PATCH', 'DELETE']
+            and not request.user.is_anonymous
+        ):
+            return request.user == obj.author or request.user.is_superuser
         return request.method in permissions.SAFE_METHODS
