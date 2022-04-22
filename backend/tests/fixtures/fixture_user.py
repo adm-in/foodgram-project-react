fe7af7816c43c155636ba249ccc6ebc69954ed67
Sergei <stagephoning@yandex.ru>
2022-04-22 11:46:52 +0300

    update tests

--- backend/tests/fixtures/fixture_user.py
+++ backend/tests/fixtures/fixture_user.py
@@ -5,7 +5,7 @@ User = get_user_model()
 
 
 @pytest.fixture()
-def user():
+def foodgram_user():
     return User.objects.create_user(
         username='test_user',
         password='!Qwerty123',
@@ -15,7 +15,26 @@ def user():
     )
 
 
-@pytest.fixture()
-def user_client(user, client):
-    client.force_login(user)
+@pytest.fixture
+def foodgram_token(foodgram_user):
+    from rest_framework.authtoken.models import Token
+
+    Token.objects.create(user=foodgram_user)
+    return Token.objects.get(user=foodgram_user)
+
+
+@pytest.fixture
+def foodgram_client(foodgram_token):
+    from rest_framework.test import APIClient
+
+    client = APIClient()
+    return client
+
+
+@pytest.fixture
+def foodgram_client_auth(foodgram_token):
+    from rest_framework.test import APIClient
+
+    client = APIClient()
+    client.credentials(HTTP_AUTHORIZATION='Token ' + foodgram_token.key)
     return client
--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -1,21 +1,30 @@
 import pytest
-from django.contrib.auth import get_user_model
-from tests.fixtures.fixture_user import user, user_client  # noqa
-
-User = get_user_model()
 
 
-@pytest.mark.django_db()
-def test_user_create():
-    User.objects.create_user(
-        username='test_user',
-        password='!Qwerty123',
-        email='test@gmail.com',
-        first_name='Test',
-        last_name='Testov',
+class TestUserLoginLogout:
+    url_register = '/api/auth/users/'
+    url_login = '/api/auth/token/login/'
+    url_logout = '/api/auth/token/logout/'
+
+    user_dict = dict(
+        username='test_user2',
+        password='!Qwerty1232',
+        email='test2@gmail.com',
+        first_name='Test2',
+        last_name='Testov2',
     )
 
-    user = User.objects.filter(username='test_user').first()
-    assert User.objects.count() == 1
-    assert isinstance(user, User)
-    assert user.check_password('!Qwerty123')
+    @pytest.mark.django_db(transaction=True)
+    def test_registration(self, foodgram_user, foodgram_client):
+        assert 'test_user' == foodgram_user.username
+        assert foodgram_user.check_password('!Qwerty123')
+        assert 'test@gmail.com' == foodgram_user.email
+        assert 'Test' == foodgram_user.first_name
+        assert 'Testov' == foodgram_user.last_name
+
+        response = foodgram_client.post(self.url_register, self.user_dict)
+
+        data = response.data
+
+        assert data['first_name'] == self.user_dict['first_name']
+        assert response.status_code == 201
