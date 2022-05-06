275541dd23e3416496d2b8542c1210490c0cca64
Sergei <stagephoning@yandex.ru>
2022-05-06 10:59:23 +0300

    add tags fixture and update conftest

---
+++ backend/tests/fixtures/fixture_tags.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture()
+def tags_list():
+    from backend.apps.recipes.models import Tag
+
+    data = [
+        {'id': 1, 'name': 'Завтрак', 'slug': 'breakfast', 'color': '#E26C2D'},
+        {'id': 2, 'name': 'Обед', 'slug': 'lunch', 'color': '#32CD32'},
+        {'id': 3, 'name': 'Ужин', 'slug': 'dinner', 'color': '#000080'},
+    ]
+    for tag in data:
+        id, name, slug, color = tag.values()
+        Tag.objects.create(id=id, name=name, slug=slug, color=color)
+    return data
